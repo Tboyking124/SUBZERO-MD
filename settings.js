088104253aa2e324b3f6caf3c0ec109e97ae0f41
Tboyking124 <tboykingosita@gmail.com>
2026-02-14 22:02:16 +0100

    Update settings.js for Hhhhh

--- settings.js
+++ settings.js
@@ -1,8 +1,6 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
-  PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
-  CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
+  SESSION_ID: "SUBZERO~7a7afb59ffaac746ce267703f2256823cd0d8ded",
+  OWNER_NUMBER: "+2348142822202", 
+  PREFIX: ".",
+  CDN: "https://mrfrankk-cdn.hf.space"
 };
-
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
